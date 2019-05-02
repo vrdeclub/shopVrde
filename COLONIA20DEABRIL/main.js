c48a20e0c1de107f02990d31ea2e4f52db26fbcc
hatechamp <hatechamp@gmail.com>
2019-05-01 21:56:34 -0300

    colonia 20 de abril

---
+++ COLONIA20DEABRIL/main.js
@@ -0,0 +1,247 @@
+//vrde.club
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        search: "",
+        price: 25,
+        discounts: discounts,
+        productList: products,
+        cartTotal: 0,
+        cart: [],
+        cartItems: 0,
+        saleComplete: false,
+        fieldsMissing: true,
+        userData: {
+            name: "",
+            address: "",
+            phone: "",
+            email: "",
+            delivery: false
+        },
+        active: {
+            'veggie': { status: true },
+            'fruit': { status: false },
+            'meal': { status: false }
+        },
+        cartHas: {
+            veggie: false,
+            fruit: false,
+            meal: false
+        }
+
+    },
+    methods: {
+        getTotal: function () {
+
+            var self = this;
+            this.cartTotal = 0;
+            this.cartItems = 0;
+
+            this.cart = this.productList.filter(function (item) {
+                return item.total > 0;
+            });
+
+            for (var item in this.cart) {
+                if (this.cart[item].type == 'veggie')
+                    this.cartItems += this.cart[item].amount;
+            }
+
+            // add discount by tier
+            if (this.cartItems <= 1) { this.price = 55 }
+            else if (this.cartItems == 2) { this.price = 52.50 }
+            else if (this.cartItems == 3) { this.price = 50 }
+            else if (this.cartItems == 4) { this.price = 47.50 }
+            else if (this.cartItems >= 5 && this.cartItems) { this.price = 45 }
+
+            for (var item in this.cart) {
+                if (this.cart[item].type == 'fruit') {
+                    this.cart[item].total = this.cart[item].amount * this.cart[item].price;
+                    this.cart[item].total = parseFloat(this.cart[item].total.toFixed(2))
+                    this.cartHas.fruit = true;
+                }
+                if (this.cart[item].type == 'veggie') {
+                    this.cart[item].price = this.price;
+                    this.cart[item].total = this.cart[item].amount * this.price;
+                    this.cartHas.veggie = true;
+                }
+                if (this.cart[item].type == "meal") {
+                    this.cart[item].total = this.cart[item].amount * this.cart[item].price;
+                    this.cart[item].total = parseFloat(this.cart[item].total.toFixed(2))
+                    this.cartHas.meal = true;
+
+                }
+                this.cartTotal += this.cart[item].total;
+                this.cartTotal = parseFloat(this.cartTotal.toFixed(2))
+            }
+
+        },
+        addItem: function (item) {
+            item.amount++;
+            if (item.price && item.type != "veggie") {
+                item.total = item.amount * item.price;
+            } else {
+                item.total = item.amount * this.price;
+            }
+            this.getTotal();
+        },
+        removeItem: function (item) {
+            this.getTotal();
+
+            if (item.amount > 0) {
+                item.amount--;
+            }
+            if (item.price && item.type != "veggie") {
+                item.total = item.amount * item.price;
+            } else {
+                console.log("Remove Item", this.price, item.price)
+                item.price = this.price;
+                item.total = item.amount * this.price;
+            }
+            this.getTotal();
+        },
+        updateValue: function (item) {
+            if (item.amount == "" || parseFloat(item.amount) == NaN) { item.amount = 0 }
+            else (item.amount = parseFloat(item.amount))
+            if (item.price) {
+                item.total = item.amount * item.price;
+            } else {
+                item.total = item.amount * this.price;
+            }
+            this.getTotal();
+        },
+        saveSale: function (cart) {
+
+            // form validation
+            if (this.userData.name == "" || this.userData.phone == "") {
+                this.fieldsMissing = true;
+                console.log(this.fieldsMissing)
+            }
+            if (this.userData.delivery == true && this.userData.address == "") {
+                this.fieldsMissing = true;
+                console.log(this.fieldsMissing)
+            }
+            else {
+                this.fieldsMissing = false;
+            }
+
+            if (this.fieldsMissing == false) {
+
+                // send to firebase
+                var today = new Date().toLocaleDateString('es-GB', {
+                    day: 'numeric',
+                    month: 'numeric',
+                    year: 'numeric'
+                }).split('/').join('-');
+
+                var sale = [{
+                    date: today,
+                    name: this.userData.name,
+                    address: this.userData.address,
+                    phone: this.userData.phone,
+                    email: this.userData.email,
+                    delivery: this.userData.delivery,
+                    total: this.cartTotal
+                }];
+
+                for (var item in cart) {
+                    sale.push({
+                        variedad: cart[item].name,
+                        cantidad: cart[item].amount,
+                        precio: cart[item].price || this.price,
+                        pago: cart[item].total
+                    })
+                }
+
+                var self = this;
+
+                database.ref('sales/' + today).push(sale, function (error) {
+                    if (error) {
+                        console.log(error)
+                    } else {
+                        self.saleComplete = true;
+                    }
+                });
+
+                database.ref('salesArchive/' + today).push(sale, function (error) {
+                    if (error) {
+                        console.log(error)
+                    } else {
+                        self.saleComplete = true;
+                    }
+                });
+                
+                
+                // ref.child("users").orderByChild("name").equalTo(sale.name).once("value",snapshot => {
+                //     if (snapshot.exists()){
+                //     const userData = snapshot.val();
+                //       var userReg = {
+                //           name: sale.name,
+                //           address: sale.address,
+                //           phone: sale.phone
+                //       }
+                //       database.ref('users').push(userReg, function(){
+                //           if (error) {
+                //               console.log(error)
+                //           } else {
+                //               console.log('User already exists') 
+                //           }
+                //       })
+                //     } else {
+                //         console.log("exists!", userData);
+                //     }
+                // });
+
+            }
+        },
+        //toggle category buttons
+        setVisibility: function (type) {
+            this.search = "";
+            for (var t in this.active) {
+                this.active[t].status = false;
+            }
+            this.active[type].status = true;
+        },
+        toggleActive: function (e) {
+            e.target.classList.add('active');
+        },
+        scrollTop: function () {
+            window.scrollTo(0, 0);
+        }
+
+    },
+    computed: {
+        // returns filtered list by search term or category
+        filteredItems: function () {
+            var self = this;
+            var newList = this.productList.sort().filter(function (item) {
+                return item.name.toLowerCase().indexOf(self.search.toLowerCase()) >= 0;
+            });
+            if (self.search != "") {
+                for (var t in this.active) {
+                    this.active[t].status = false;
+                }
+                for (var i in newList) {
+                    self.active[newList[i].type].status = true;
+                }
+            }
+            var input = document.getElementById('searchInput');
+
+            input.onkeyup = function () {
+                var key = event.keyCode || event.charCode;
+
+                if (key == 8 || key == 46 && self.search == "") {
+                    self.active = {
+                        'veggie': { status: true },
+                        'fruit': { status: false },
+                        'meal': { status: false }
+                    }
+                }
+            };
+
+            return newList.filter(function (item) {
+                return self.active[item.type].status == true;
+            }).sort();
+        }
+    }
+})
---
+++ COLONIA20DEABRIL/products.js
@@ -0,0 +1,71 @@
+let products = [
+    { name: 'Acelga (kg)', amount: 0, url: 'images/acelga.jpg', type: 'veggie' },
+    // { name: 'Achicoria (500g)', amount: 0, url: 'images/achicoria.jpg', type: 'veggie' },
+    // { name: 'Aji Vinagre (500g)', amount: 0, url: 'images/ajivinagre.jpg', type: 'veggie' },
+    { name: 'Ajo (x3)', amount: 0, url: 'images/ajo.jpg', type: 'veggie' },
+    { name: 'Albahaca (x2)', amount: 0, url: 'images/albahaca.jpg', type: 'veggie' },
+    { name: 'Anco (kg)', amount: 0, url: 'images/anco.jpg', type: 'veggie' },
+    { name: 'Apio (kg)', amount: 0, url: 'images/apio.jpg', type: 'veggie' },
+    { name: 'Batata (kg)', amount: 0, url: 'images/batata.jpg', type: 'veggie' },
+    // { name: 'Brocoli (kg)', amount: 0, url: 'images/brocoli.jpg', type: 'veggie' },
+    { name: 'Berenjena (kg)', amount: 0, url: 'images/berenjena.jpg', type: 'veggie' },
+    { name: 'Cebolla (kg)', amount: 0, url: 'images/cebolla.jpg', type: 'veggie' },
+    { name: 'Chaucha (kg)', amount: 0, url: 'images/chaucha.jpg', type: 'veggie' },
+    { name: 'Choclo x 3', amount: 0, url: 'images/choclo.jpg', type: 'veggie' },
+    // { name: 'Espinaca (500g)', amount: 0, url: 'images/espinaca.jpg', type: 'veggie' },
+    { name: 'Hinojo (Paq)', amount: 0, url: 'images/hinojo.jpg', type: 'veggie', price: 25 },
+    // { name: 'Kale Morado (500g)', amount: 0, url: 'images/kalemorado.jpg', type: 'veggie' },
+    { name: 'Kale (500g)', amount: 0, url: 'images/kale.jpg', type: 'veggie' },
+    { name: 'Lechuga Crespa(500g)', amount: 0, url: 'images/lechugamantecosa.jpg', type: 'veggie' },
+    // { name: 'Lechuga Mantecosa (1/2kg)', amount: 0, url: 'images/lechugacrespa.jpg', type: 'veggie' },
+    // { name: 'Mijo (500g)', amount: 0, url: 'images/mijo.jpg', type: 'veggie' },
+    { name: 'Morron Verde(kg)', amount: 0, url: 'images/morron.jpg', type: 'veggie' },
+    { name: 'Morron Rojo(kg)', amount: 0, url: 'images/morronrojo.jpg', type: 'veggie' },
+    { name: 'Nabo (kg)', amount: 0, url: 'images/nabo.jpg', type: 'veggie' },
+    // { name: 'Pack Choi (kg)', amount: 0, url: 'images/packchoi.jpg', type: 'veggie' },
+    { name: 'Papa (kg)', amount: 0, url: 'images/papa.jpg', type: 'veggie' },
+    // { name: 'Papa Colorada (kg)', amount: 0, url: 'images/papacolorada.jpg', type: 'veggie' },
+    { name: 'Pepino (kg)', amount: 0, url: 'images/pepino.jpg', type: 'veggie' },
+    { name: 'Perejil (400g)', amount: 0, url: 'images/perejil.jpg', type: 'veggie' },
+    { name: 'Puerro (PAQ)', amount: 0, url: 'images/puerro.jpg', type: 'veggie' },
+    { name: 'Rabanito (500g)', amount: 0, url: 'images/rabanito.jpg', type: 'veggie' },
+    // { name: 'Radicheta (500g)', amount: 0, url: 'images/radicheta.jpg', type: 'veggie' },
+    { name: 'Repollo (kg)', amount: 0, url: 'images/repollo.jpg', type: 'veggie' },
+    { name: 'Remolacha (kg)', amount: 0, url: 'images/remolacha.jpg', type: 'veggie' },
+    { name: 'Rucula (500g)', amount: 0, url: 'images/rucula.jpg', type: 'veggie' },
+    // { name: 'Tomate (kg)', amount: 0, url: 'images/tomato.jpg', type: 'veggie' },
+    // { name: 'Tomate Cherry (500g)', amount: 0, url: 'images/tomatecherry.jpg', type: 'veggie' },
+    { name: 'Verdeo (500g)', amount: 0, url: 'images/verdeo.jpg', type: 'veggie' },
+    { name: 'Zanahoria (kg)', amount: 0, url: 'images/zanahoria.jpg', type: 'veggie' },
+    { name: 'Zapallito (kg)', amount: 0, url: 'images/zapallitos.jpg', type: 'veggie' },
+    { name: 'Zuchinni (kg)', amount: 0, url: 'images/zuchinni.jpg', type: 'veggie' },
+
+]
+products = products.sort()
+
+let discounts = [
+    // { amount: "1", price: 55 },
+    //{ amount: "2", price: 52.50 },
+    //{ amount: "3", price: 50 },
+    //{ amount: "4", price: 47.50 },
+    // { amount: "5+", price: 45 },
+    //{ amount: "60 > 200", price: 41 },
+    //{ amount: "200 > 250", price: 36 },
+    //{ amount: "250 >", price: 32 }
+]
+
+
+//Scroll top on pageload
+window.addEventListener('scroll', function (evt) {
+
+    var distance_from_top = document.documentElement.scrollTop
+
+    if (distance_from_top === 0) {
+        document.getElementsByClassName("search")[0].classList.remove("fixed");
+    }
+
+    if (distance_from_top > 0) {
+        document.getElementsByClassName("search")[0].classList.add("fixed");
+    }
+
+});
